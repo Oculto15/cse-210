132fbb95a7aa959e072456677dcdb82b3dcad0f6
Oculto15 <93210080+Oculto15@users.noreply.github.com>
2022-06-22 10:57:22 -0600

    update

--- cse210-student-articulate-csharp-main 2/Unit05/Game/Scripting/MoveActorsAction.cs
+++ cse210-student-articulate-csharp-main 2/Unit05/Game/Scripting/MoveActorsAction.cs
@@ -8,6 +8,8 @@ namespace Unit05.Game.Scripting
 
     // 1) Add the class declaration. Use the following class comment. Make sure you
     //    inherit from the Action class.
+    public class MoveActorsAction : Action{
+    
 
     /// <summary>
     /// <para>An update action that moves all the actors.</para>
@@ -18,14 +20,27 @@ namespace Unit05.Game.Scripting
 
     // 2) Create the class constructor. Use the following method comment.
 
+    public MoveActorsAction(){
+        
+    }
+
     /// <summary>
     /// Constructs a new instance of MoveActorsAction.
     /// </summary>
+
+
 
     // 3) Override the Execute(Cast cast, Script script) method. Use the following 
     //    method comment. You custom implementation should do the following:
     //    a) get all the actors from the cast
     //    b) loop through all the actors
     //    c) call the MoveNext() method on each actor.
-
+     public void Execute(Cast cast, Script script)
+        {
+            List<Actor> actors = cast.GetAllActors();
+            foreach( Actor actor in actors){
+                actor.MoveNext();
+            };
+        }
+    }
 }
